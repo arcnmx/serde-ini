74aa8824055167b96e7f4d0f8cd7a9ee8752d431
arcnmx <arcnmx@users.noreply.github.com>
2017-11-20 18:19:02 -0500

    add serialization and map/sections to smoke test

---
+++ tests/smoke.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_ini;
+
+use serde::{Deserialize, Serialize};
+use serde_ini::{Deserializer, Serializer, Parser, Writer, LineEnding};
+
+#[derive(Deserialize, Serialize, Clone, PartialEq, Default, Debug)]
+struct TestModel {
+    key1: String,
+    key2: u32,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    map1: Option<Box<TestModel>>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    map2: Option<Box<TestModel>>,
+}
+
+const TEST_INPUT: &'static str = "
+; Ignored comment
+key1=value1
+key2=255
+
+[map1]
+key2=256
+key1=value2
+
+[map2]
+key1=value3
+key2=257
+";
+
+fn expected() -> TestModel {
+    TestModel {
+        key1: "value1".into(),
+        key2: 255,
+        map1: Some(Box::new(TestModel {
+            key1: "value2".into(),
+            key2: 256,
+            .. Default::default()
+        })),
+        map2: Some(Box::new(TestModel {
+            key1: "value3".into(),
+            key2: 257,
+            .. Default::default()
+        })),
+    }
+}
+
+#[test]
+fn smoke_de() {
+    assert_eq!(expected(), TestModel::deserialize(&mut Deserializer::new(Parser::from_str(TEST_INPUT))).unwrap());
+
+    assert_eq!(expected(), TestModel::deserialize(&mut Deserializer::new(Parser::from_read(TEST_INPUT.as_bytes()))).unwrap());
+}
+
+#[test]
+fn smoke_en() {
+    let model = expected();
+
+    let mut data = Vec::<u8>::new();
+    model.serialize(&mut Serializer::new(Writer::new(&mut data, LineEnding::default()))).unwrap();
+
+    assert_eq!(model, TestModel::deserialize(&mut Deserializer::new(Parser::from_read(&data[..]))).unwrap());
+}
