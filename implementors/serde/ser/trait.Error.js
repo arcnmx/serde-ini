19bc3990ee2228c219d42e675313ab96b03296f7
ghost <ghost@konpa.ku>
2017-12-19 21:08:05 +0000

    0.1.3 - 85d5acaa0c97f468346adfe20af452b3627e46b9

--- implementors/serde/ser/trait.Error.js
+++ implementors/serde/ser/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["serde_ini"] = ["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.21/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_ini/error/enum.Error.html\" title=\"enum serde_ini::error::Error\">Error</a>","impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.21/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_ini/ser/enum.Error.html\" title=\"enum serde_ini::ser::Error\">Error</a>",];
+implementors["serde_ini"] = ["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.24/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_ini/error/enum.Error.html\" title=\"enum serde_ini::error::Error\">Error</a>","impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.24/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_ini/ser/enum.Error.html\" title=\"enum serde_ini::ser::Error\">Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/serde/ser/trait.Serializer.js
+++ implementors/serde/ser/trait.Serializer.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["serde_ini"] = ["impl&lt;'a, W:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.21/serde/ser/trait.Serializer.html\" title=\"trait serde::ser::Serializer\">Serializer</a> for &amp;'a mut <a class=\"struct\" href=\"serde_ini/ser/struct.Serializer.html\" title=\"struct serde_ini::ser::Serializer\">Serializer</a>&lt;W&gt;",];
+implementors["serde_ini"] = ["impl&lt;'a, W:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.24/serde/ser/trait.Serializer.html\" title=\"trait serde::ser::Serializer\">Serializer</a> for &amp;'a mut <a class=\"struct\" href=\"serde_ini/ser/struct.Serializer.html\" title=\"struct serde_ini::ser::Serializer\">Serializer</a>&lt;W&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
